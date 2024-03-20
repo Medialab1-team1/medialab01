8daad12e34b6bce8dc57344d6a5c6b7f0797db39
Pim van Milt <pim.van.milt@dpdk.com>
2024-03-20 17:35:27 +0100

    popup refactor

--- medialab01/src/components/UploadPopup/UploadPopup.jsx
+++ medialab01/src/components/UploadPopup/UploadPopup.jsx
@@ -2,5 +2,5 @@ import clsx from "clsx";
 import styles from "./UploadPopup.module.css";
 
 export default function UploadPopup({ active }) {
-  return <div className={clsx([styles.popupContainer])}></div>;
+  return <div className={clsx([styles.popupContainer])}>Where do i go?</div>;
 }
