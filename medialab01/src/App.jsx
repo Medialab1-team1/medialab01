25e9a662b791582f065f865b4ad3f200f1557cb5
Pim van Milt <pim.van.milt@dpdk.com>
2024-03-20 17:17:47 +0100

    App changes

--- medialab01/src/App.jsx
+++ medialab01/src/App.jsx
@@ -1,8 +1,10 @@
 // import
 import { useState } from "react";
+import { FocusOn } from "react-focus-on";
+import clsx from "clsx";
 
 // import components
-import UploadPopup from "./components/UploadPopup";
+import UploadPopup from "./components/UploadPopup/UploadPopup";
 
 // import css
 import "./App.css";
@@ -15,9 +17,32 @@ function App() {
       <header className="App-header">Let there be a menu here</header>
       <main className="App-main">
         {/* request data to be uploaded when flag is set to true */}
-        {requestDataUpload && <UploadPopup />}
+        {requestDataUpload && (
+          <div className="App-popup">
+            <FocusOn
+              enabled={requestDataUpload}
+              onEscapeKey={() => {
+                console.log(`escape`);
+                setRequestDataUpload((v) => !v);
+              }}
+              onClickOutside={() => {
+                console.log(`click`);
+                setRequestDataUpload((v) => !v);
+              }}
+              gapMode="mpadding"
+            >
+              <UploadPopup
+                active={requestDataUpload}
+                setRequestDataUpload={setRequestDataUpload}
+              />
+            </FocusOn>
+          </div>
+        )}
         {/* rest of page follows here*/}
         Let there be a dashboard here
+        <button onClick={() => setRequestDataUpload((v) => !v)}>
+          bring popup back
+        </button>
       </main>
       <footer className="App-footer">and I'm a footer</footer>
     </div>
