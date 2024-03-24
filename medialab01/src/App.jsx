c56dfc9d925e4f514a7facc158b2ab0268da980f
Kevin <89836835+Kaasstengel28@users.noreply.github.com>
2024-03-24 22:36:49 +0100

    made a graph, not yet in a component. does not yet work with csv data.

--- medialab01/src/App.jsx
+++ medialab01/src/App.jsx
@@ -1,11 +1,47 @@
 // import
-import { useState } from "react";
+import React, { useState } from "react";
+import {Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
 
 // import components
 import UploadPopup from "./components/UploadPopup";
+// import GraphBox from "./components/GraphBox";
 
 // import css
 import "./App.css";
+
+//fake data for now
+const data = [
+    {
+        month: 'January',
+        savings: 5000,
+        loss: 500
+    },
+    {
+        month: 'February',
+        savings: 8000,
+        loss: 300
+    },
+    {
+        month: 'March',
+        savings: 3000,
+        loss: 800
+    },
+    {
+        month: 'April',
+        savings: 6000,
+        loss: 100
+    },
+    {
+        month: 'May',
+        savings: 4000,
+        loss: 700
+    },
+    {
+        month: 'June',
+        savings: 9000,
+        loss: 1200
+    },
+]
 
 function App() {
   const [requestDataUpload, setRequestDataUpload] = useState(true);
@@ -17,8 +53,17 @@ function App() {
         {/* request data to be uploaded when flag is set to true */}
         {requestDataUpload && <UploadPopup />}
         {/* rest of page follows here*/}
-        Let there be a dashboard here
-      </main>
+        {/*  temp graph not in component cuz shits giving me headaches*/}
+          <h1>Simple Line chart</h1>
+          <LineChart width={1000} height={300} data={data}>
+              <XAxis dataKey="month"></XAxis>
+              <YAxis></YAxis>
+              <Tooltip> </Tooltip>
+              <Legend></Legend>
+              <Line type="monotone" dataKey="loss" stroke="green" />
+              <Line type="monotone" dataKey="savings" stroke="red"/>
+          </LineChart>
+            </main>
       <footer className="App-footer">and I'm a footer</footer>
     </div>
   );
