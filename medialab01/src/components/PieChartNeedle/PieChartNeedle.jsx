1b8f8c6effcd811eb7f0606fcbd083a285e9e6ab
Admin <mistermustache33@gmail.com>
2024-03-25 16:43:37 +0100

    working on the needle

--- medialab01/src/components/PieChartNeedle/PieChartNeedle.jsx
+++ medialab01/src/components/PieChartNeedle/PieChartNeedle.jsx
@@ -1,21 +1,25 @@
-/* eslint-disable no-shadow */
 import React, { PureComponent } from 'react';
 import { PieChart, Pie, Cell } from 'recharts';
+import { DataContext } from '../../contexts/DataContext';
 
 const RADIAN = Math.PI / 180;
 const data = [
-  { name: 'A', value: 80, color: 'red' },
-  { name: 'B', value: 45, color: 'orange' },
-  { name: 'C', value: 25, color: 'green' },
+  { name: 'A', value: 80, color: '#ff0000' },
+  { name: 'B', value: 45, color: '#00ff00' },
+  { name: 'C', value: 25, color: '#0000ff' },
 ];
 const cx = 150;
 const cy = 200;
 const iR = 50;
 const oR = 100;
-const value = 50;
+let value = 50; // position of needle
+
+data.forEach((segment) => {
+  value += segment.value;
+});
 
 const needle = (value, data, cx, cy, iR, oR, color) => {
-  let total = 0;
+  let total = 100; // how far the needle goes (i think)
   data.forEach((v) => {
     total += v.value;
   });
