c1de7c212931323da1aa8dfed5a68c7888b1530a
Kevin <89836835+Kaasstengel28@users.noreply.github.com>
2024-03-26 14:27:47 +0100

    graph is component, csv dat does not show

--- medialab01/src/App.jsx
+++ medialab01/src/App.jsx
@@ -1,47 +1,12 @@
 // import
 import React, { useState } from "react";
-import {Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
 
 // import components
 import UploadPopup from "./components/UploadPopup";
-// import GraphBox from "./components/GraphBox";
+import GraphBox from "./components/GraphBox";
 
 // import css
 import "./App.css";
-
-//fake data for now
-const data = [
-    {
-        month: 'January',
-        savings: 5000,
-        loss: 500
-    },
-    {
-        month: 'February',
-        savings: 8000,
-        loss: 300
-    },
-    {
-        month: 'March',
-        savings: 3000,
-        loss: 800
-    },
-    {
-        month: 'April',
-        savings: 6000,
-        loss: 100
-    },
-    {
-        month: 'May',
-        savings: 4000,
-        loss: 700
-    },
-    {
-        month: 'June',
-        savings: 9000,
-        loss: 1200
-    },
-]
 
 function App() {
   const [requestDataUpload, setRequestDataUpload] = useState(true);
@@ -55,14 +20,7 @@ function App() {
         {/* rest of page follows here*/}
         {/*  temp graph not in component cuz shits giving me headaches*/}
           <h1>Simple Line chart</h1>
-          <LineChart width={1000} height={300} data={data}>
-              <XAxis dataKey="month"></XAxis>
-              <YAxis></YAxis>
-              <Tooltip> </Tooltip>
-              <Legend></Legend>
-              <Line type="monotone" dataKey="loss" stroke="green" />
-              <Line type="monotone" dataKey="savings" stroke="red"/>
-          </LineChart>
+          <GraphBox />
             </main>
       <footer className="App-footer">and I'm a footer</footer>
     </div>
--- medialab01/src/components/GraphBox.jsx
+++ medialab01/src/components/GraphBox.jsx
@@ -1,51 +1,12 @@
-import React from "react";
-import {
-    Line,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    LineChart,
-} from "recharts";
+import React, { useEffect, useState } from "react";
+import { Line, LineChart, Tooltip, XAxis, YAxis, Legend } from "recharts";
+import Papa from "papaparse";
 
-
-export default function GraphBox() {
-    const data = [
-        {
-            month: 'January',
-            savings: 5000,
-            loss: 500
-        },
-        {
-            month: 'February',
-            savings: 8000,
-            loss: 300
-        },
-        {
-            month: 'March',
-            savings: 3000,
-            loss: 800
-        },
-        {
-            month: 'April',
-            savings: 6000,
-            loss: 100
-        },
-        {
-            month: 'May',
-            savings: 4000,
-            loss: 700
-        },
-        {
-            month: 'June',
-            savings: 9000,
-            loss: 1200
-        },
-    ]
+const GraphBox = () => {
+    const [data, setData] = useState([]);
 
     return (
-        <>
+        <div>
             <h1>Simple Line chart</h1>
             <LineChart width={1000} height={300} data={data}>
                 <XAxis dataKey="month"></XAxis>
@@ -53,9 +14,12 @@ export default function GraphBox() {
                 <Tooltip> </Tooltip>
                 <Legend></Legend>
                 <Line type="monotone" dataKey="loss" stroke="green" />
-                <Line type="monotone" dataKey="savings" stroke="red"/>
+                <Line type="monotone" dataKey="savings" stroke="red" />
             </LineChart>
-        </>
-    )
-}
+        </div>
+    );
+};
+
+export default GraphBox;
+
 
